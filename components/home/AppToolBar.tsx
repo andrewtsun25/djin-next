e22051bc27e69da970538df4cab9437d1722ceae
Andrew Tsun <andrew.h.tsun@alumni.usc.edu>
2022-12-16 16:56:34 -0800

    [feature]: Added app toolbar with correct styling & tests

---
+++ components/home/AppToolBar.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import {
+  AppBar,
+  Box,
+  IconButton,
+  Theme,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import Link from "next/link";
+import logo from "../../public/logos/logo.png";
+import Image from "next/image";
+import { useTheme } from "@mui/system";
+
+interface AppToolBarProps {
+  isAppDrawerOpen: boolean;
+  setIsAppDrawerOpen(newIsDrawerOpen: boolean): void;
+}
+
+export const MENU_BUTTON_ROLE = "switch";
+
+const AppToolBar: React.FC<AppToolBarProps> = ({
+  isAppDrawerOpen,
+  setIsAppDrawerOpen,
+}: AppToolBarProps) => {
+  const theme: Theme = useTheme();
+  console.log("Theme = ", theme);
+  return (
+    <AppBar position="sticky">
+      <Toolbar>
+        <IconButton
+          color="inherit"
+          aria-label="open drawer"
+          role={MENU_BUTTON_ROLE}
+          onClick={() => setIsAppDrawerOpen(true)}
+          edge="start"
+          sx={{ mr: 2, ...(isAppDrawerOpen && { display: "none" }) }}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Link href="/">
+          <Box height="100%" display="flex" alignItems="center">
+            <Image src={logo} alt="d.jin website logo" width={50} height={50} />
+            <Typography variant="h6" ml={1}>
+              d.jin
+            </Typography>
+          </Box>
+        </Link>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default AppToolBar;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,17 +2,24 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
 // Material UI
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-import {CssBaseline} from "@mui/material";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import { CssBaseline } from "@mui/material";
+import { useState } from "react";
+import { AppToolBar } from "../components/home";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [isAppDrawerOpen, setIsAppDrawerOpen] = useState(false);
   return (
     <>
-        <CssBaseline/>
-        <Component {...pageProps} />
+      <CssBaseline />
+      <AppToolBar
+        isAppDrawerOpen={isAppDrawerOpen}
+        setIsAppDrawerOpen={setIsAppDrawerOpen}
+      />
+      <Component {...pageProps} />
     </>
   );
 }
