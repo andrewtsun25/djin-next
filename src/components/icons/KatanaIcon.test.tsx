3a13b5628f0fdb4950f27c4d53489c73519a250b
Andrew Tsun <andrew.h.tsun@alumni.usc.edu>
2022-12-17 14:57:48 -0800

    [test]: Add tests across all components

---
+++ src/components/icons/KatanaIcon.test.tsx
@@ -0,0 +1,9 @@
+import {render} from "@testing-library/react";
+import {KatanaIcon} from "./KatanaIcon";
+
+describe("KatanaIcon", () => {
+  it("renders", () => {
+    const fistIcon = render(<KatanaIcon />);
+    expect(fistIcon).toMatchSnapshot();
+  })
+})
---
+++ src/components/icons/TaekwondoIcon.test.tsx
@@ -0,0 +1,9 @@
+import { render } from "@testing-library/react";
+import { TaekwondoIcon } from "./TaekwondoIcon";
+
+describe("TaekwondoIcon", () => {
+  it("renders", () => {
+    const fistIcon = render(<TaekwondoIcon />);
+    expect(fistIcon).toMatchSnapshot();
+  });
+});
