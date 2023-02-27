3360b480bbd119dc9b2c7346ccf1fcd6fb187bd3
Andrew Tsun <andrew.h.tsun@alumni.usc.edu>
2023-02-26 19:39:30 -0800

    Create new "AutoComplete" multi-select

--- src/components/employment/EmploymentTypeSelect.tsx
+++ src/components/employment/EmploymentTypeSelect.tsx
@@ -1,20 +1,10 @@
-import {
-  Box,
-  Checkbox,
-  Chip,
-  FormControl,
-  InputLabel,
-  ListItemText,
-  MenuItem,
-  OutlinedInput,
-  Select,
-  SelectChangeEvent,
-} from "@mui/material";
 import React from "react";
 import { JobType } from "../../types/api";
-import { EmploymentText, EmploymentTypeSelectContainer } from "./styled";
+import { MultiSelect } from "../select";
+import { identity } from "lodash";
 
 interface EmploymentTypeSelectProps {
+  employmentTypes: JobType[];
   employmentTypesSelected: JobType[];
   setEmploymentTypesSelected(jobType: JobType[]): void;
 }
@@ -31,54 +21,23 @@ const MenuProps = {
   },
 };
 
+const employmentTypes: JobType[] = [JobType.FullTime, JobType.Internship];
 const EmploymentTypeSelect: React.FC<EmploymentTypeSelectProps> = ({
   employmentTypesSelected,
   setEmploymentTypesSelected,
 }: EmploymentTypeSelectProps) => {
-  const handleChange = ({
-    target: { value: newEmploymentTypesSelectedAsString },
-  }: SelectChangeEvent<JobType[]>) => {
-    const newEmploymentTypesSelected: JobType[] =
-      typeof newEmploymentTypesSelectedAsString === "string"
-        ? newEmploymentTypesSelectedAsString
-            .split(",")
-            .map((employmentType) => employmentType as JobType)
-        : newEmploymentTypesSelectedAsString;
-    setEmploymentTypesSelected(newEmploymentTypesSelected);
+  const handleChange = (_: React.SyntheticEvent, value: string[]) => {
+    setEmploymentTypesSelected(value.map((v) => v as JobType));
   };
   return (
-    <FormControl>
-      <InputLabel id="demo-multiple-checkbox-label">Employment Type</InputLabel>
-      <Select
-        labelId="employment-type-selection-label"
-        id="employment-type-selection"
-        multiple
-        value={employmentTypesSelected}
-        onChange={handleChange}
-        input={<OutlinedInput label="Types of Employment" />}
-        renderValue={(selected: JobType[]) => (
-          <Box sx={{ display: "flex", flexWrap: "wrap", gap: 0.5 }}>
-            {selected.map((value: JobType) => (
-              <Chip key={value} label={value} />
-            ))}
-          </Box>
-        )}
-        MenuProps={MenuProps}
-      >
-        <MenuItem key={JobType.FullTime} value={JobType.FullTime}>
-          <Checkbox
-            checked={employmentTypesSelected.indexOf(JobType.FullTime) > -1}
-          />
-          <ListItemText primary={JobType.FullTime} />
-        </MenuItem>
-        <MenuItem key={JobType.Internship} value={JobType.Internship}>
-          <Checkbox
-            checked={employmentTypesSelected.indexOf(JobType.Internship) > -1}
-          />
-          <ListItemText primary={JobType.Internship} />
-        </MenuItem>
-      </Select>
-    </FormControl>
+    <MultiSelect
+      sx={{ width: 350 }}
+      options={employmentTypes}
+      value={employmentTypesSelected}
+      getOptionLabel={identity}
+      inputLabel="Employment Type"
+      onChange={handleChange}
+    />
   );
 };
 
