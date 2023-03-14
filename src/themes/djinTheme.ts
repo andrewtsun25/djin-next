fb88e7f8e34bada185a2994f402ad2acf21ba924
Andrew Tsun <andrew.h.tsun@alumni.usc.edu>
2023-03-13 22:51:30 -0700

    [styling]: Modernize theme to be dark, use Apple fonts

---
+++ src/themes/djinTheme.ts
@@ -0,0 +1,41 @@
+import { createTheme, Theme } from "@mui/material";
+import { green, red } from "@mui/material/colors";
+
+const djinTheme: Theme = createTheme({
+  palette: {
+    primary: {
+      light: "#69696a",
+      main: "#28282a",
+      dark: "#1e1e1f",
+    },
+    secondary: {
+      light: "#fff5f8",
+      main: "#ff3366",
+      dark: "#e62958",
+    },
+    warning: {
+      main: "#ffc071",
+      dark: "#ffb25e",
+    },
+    error: {
+      light: red[50],
+      main: red[500],
+      dark: red[700],
+    },
+    success: {
+      light: green[50],
+      main: green[500],
+      dark: green[700],
+    },
+  },
+  typography: {
+    fontFamily: [
+      '"Helvetica Neue"',
+      "-apple-system",
+      "BlinkMacSystemFont",
+      "roboto",
+    ].join(),
+  },
+});
+
+export default djinTheme;
