f11d6566c9873e948912641baece137c7ac60b69
Andrew Tsun <andrew.h.tsun@alumni.usc.edu>
2023-04-24 01:12:59 -0700

    [config]: Edit deployment message for ignored builds

--- vercel_ignored_build_step.sh
+++ vercel_ignored_build_step.sh
@@ -6,10 +6,10 @@ echo "VERCEL_GIT_COMMIT_AUTHOR_LOGIN: $VERCEL_GIT_COMMIT_AUTHOR_LOGIN"
 
 if [[ "$VERCEL_GIT_COMMIT_AUTHOR_LOGIN" == "$DEPENDABOT_AUTHOR_NAME" ]] ; then
   # Skip all builds authored by dependabot
-  echo "🛑 - Build cancelled"
+  echo "🛑 - Build authored by dependabot, cancelling build..."
   exit 0;
 else
   # Proceed with build otherwise
-  echo "✅ - Build can proceed"
+  echo "✅ - Build not authored by dependabot, initiating build..."
   exit 1;
 fi
