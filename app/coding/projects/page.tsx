45126c7af9e10927756e7ca2c2102fe740729904
Andrew Tsun <andrew.h.tsun@alumni.usc.edu>
2024-02-28 18:10:22 -0800

    [migration]: Migrate projects tab

---
+++ app/coding/projects/page.tsx
@@ -0,0 +1,29 @@
+import { Metadata } from "next";
+import Head from "next/head";
+import React from "react";
+
+import { ProjectSelection } from "../../../src/components/projects";
+import {
+  ProjectsBackground,
+  ProjectsPageTitle,
+} from "../../../src/components/projects/styled";
+import { listProjects } from "../../../src/dal/api";
+
+export const metadata: Metadata = {
+  title: "d.jin - Coding Projects",
+};
+
+export default async function ProjectsPage() {
+  const projects = await listProjects();
+  return (
+    <>
+      <Head>
+        <title>d.jin - Coding Projects</title>
+      </Head>
+      <ProjectsBackground>
+        <ProjectsPageTitle variant="h2">Coding Projects</ProjectsPageTitle>
+        <ProjectSelection projects={projects} />
+      </ProjectsBackground>
+    </>
+  );
+}
--- src/components/projects/styled/ProjectCardMedia.tsx
+++ src/components/projects/styled/ProjectCardMedia.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { CardMedia } from "@mui/material";
 import { styled } from "@mui/material/styles";
 
